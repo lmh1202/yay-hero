d18dd267c1a351ecf3177b4edd289f44fe6f4fc0
lmh1202 <lmhao1202@gmail.com>
2023-03-31 14:30:51 +0700

    Done Anchor

--- apps/heroes/src/components/settings/SettingsAnchor.tsx
+++ apps/heroes/src/components/settings/SettingsAnchor.tsx
@@ -1,5 +1,31 @@
-function SettingsAnchor() {
-    return <h2>Anchor</h2>
+import { Anchor } from "antd"
+
+type Props = {
+    defaultValuesComponentID: string
+    levelUpAttributesComponentID: string
+}
+
+function SettingsAnchor({ defaultValuesComponentID, levelUpAttributesComponentID }: Props) {
+    return (
+        <div id="anchor">
+            <Anchor
+                onClick={(e) => e.preventDefault()}
+                offsetTop={50}
+                items={[
+                    {
+                        key: defaultValuesComponentID,
+                        href: `#${defaultValuesComponentID}`,
+                        title: 'Default Value',
+                    },
+                    {
+                        key: levelUpAttributesComponentID,
+                        href: `#${levelUpAttributesComponentID}`,
+                        title: 'Level Up Attributes',
+                    },
+                ]}
+            />
+        </div>
+    )
 }
 
 export default SettingsAnchor
--- apps/heroes/src/pages/SettingsPage.tsx
+++ apps/heroes/src/pages/SettingsPage.tsx
@@ -2,25 +2,45 @@ import DefaultValuesCollapse from "@src/components/settings/default-values/Defau
 import DefaultValuesModal from "@src/components/settings/default-values/DefaultValuesModal"
 import LevelUpAttributesCollapse from "@src/components/settings/levelup-attributes/LevelUpAttributesCollapse"
 import LevelUpAttributesModal from "@src/components/settings/levelup-attributes/LevelUpAttributesModal"
-import { Button } from "antd"
+import SettingsAnchor from "@src/components/settings/SettingsAnchor"
+import { Button, Col, Row } from "antd"
 import { useNavigate } from "react-router-dom"
 
 function SettingsPage() {
     const navigate = useNavigate()
 
+    const defaultValuesComponentID = "defaultValue"
+
+    const levelUpAttributesComponentID = "levelUp"
+
     return (
         <div>
             <h1>Settings</h1>
-            <Button type="primary" onClick={() => navigate(-1)} style={{ marginBottom: 10 }}>
-                Back
-            </Button>
-            {/* DefaultValue */}
-            <DefaultValuesCollapse />
-            <DefaultValuesModal />
-            {/* LevelAttributes */}
-            <LevelUpAttributesCollapse />
-            <LevelUpAttributesModal />
-        </div>
+            <Row>
+                <Col span={21}>
+                    <Button type="primary" onClick={() => navigate(-1)} style={{ marginBottom: 10 }}>
+                        Back
+                    </Button>
+                    {/* DefaultValue */}
+                    <div id={defaultValuesComponentID}>
+                        <DefaultValuesCollapse />
+                        <DefaultValuesModal />
+                    </div>
+
+                    {/* LevelAttributes */}
+                    <div id={levelUpAttributesComponentID}>
+                        <LevelUpAttributesCollapse />
+                        <LevelUpAttributesModal />
+                    </div>
+                </Col>
+                <Col span={3}>
+                    <SettingsAnchor
+                        defaultValuesComponentID={defaultValuesComponentID}
+                        levelUpAttributesComponentID={levelUpAttributesComponentID}
+                    />
+                </Col>
+            </Row>
+        </div >
     )
 }
 
